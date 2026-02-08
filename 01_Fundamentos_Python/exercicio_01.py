051866bf7252d08a52f288e758560c961c434a7f
maximiliano leonel <maxvargasdev@gmail.com>
2026-02-08 09:35:27 -0300

    Adicionando primeiro exercício de lógica financeira

---
+++ 01_Fundamentos_Python/exercicio_01.py
@@ -0,0 +1,11 @@
+nome_fundo = "HGLG11"
+quantidade_cotas = 50
+preço_cota = 160.00
+dividendo = 1.10
+
+valor_total = quantidade_cotas * preço_cota
+valor_dividendo = quantidade_cotas * dividendo
+
+print(f"fundo: {nome_fundo}")
+print(f"Total Investido: R7:{valor_total}")
+print(f"Total dividendo recibidos: R$ {valor_dividendo}")
